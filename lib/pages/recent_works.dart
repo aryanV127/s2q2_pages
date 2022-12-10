9c9072c5d7cfba083f384697ef05743b69b86260
aryanV127 <aryan@s2q2.com>
2022-12-10 19:15:40 +0530

    screen is completed

--- lib/pages/recent_works.dart
+++ lib/pages/recent_works.dart
@@ -8,25 +8,74 @@ class RecentWork extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: 400,
-        height: 400,
-        child: Card(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FlutterLogo(
-                size: 45,
+        child: Row(
+          children: [
+            const MyContainer(
+              text:
+                  'US Based Startup for Clubs and Restaurants, for which we have build IoS App.',
+            ),
+            const MyContainer(
+              text:
+                  'A Startup based at IIT Bombay that works in the Medical Science.',
+            ),
+            const MyContainer(
+              text:
+                  'A FinTech marketplace with excellent financial offers on Loan, Insurance and Credit Cards.',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        // alignment: Alignment.bottomRight,
+        width: 300,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Row(
+          children: [
+            const SizedBox(height: 20),
+            Container(
+              // backgroundColor: Colors.,
+              child: Image.asset(
+                'images/p2.jpg',
+                height: 300,
+                width: 150,
+                fit: BoxFit.fitHeight,
               ),
-              Container(
-                color: Colors.red,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                color: Colors.white,
                 child: Text(
-                  "US Based Startup for Clubs and Restaurants, for which we have build IoS App.",
-                  softWrap: false,
-                  maxLines: 4,
+                  text,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w100,
+                    color: Colors.black,
+                  ),
+                  // maxLines: 1,
                 ),
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
